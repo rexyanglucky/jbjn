f2bc44032364567127856e1712d67769aa4b2129
rex yang <rexyangluck@live.com>
2018-07-24 11:13:49 +0800

    添加产品详情页和联系我们

--- routes/index.js
+++ routes/index.js
@@ -49,6 +49,46 @@ router.get('/plist', function (req, res, next) {
       res.render('common/layout', data);
     });
 });
+router.get('/product', function (req, res, next) {
+  let pcDal = new ProductCategoryDal();
+  let pDal = new ProductDal();
+  let data = { pList: [], url: req.path,cList:[],category:{}};
+  let pid = req.query.id || 1;
+  pcDal.Select(0, 100).then(
+    info => {
+      data.cList = info;
+      return pDal.getProductByID(pid);
+    }).then(r => {
+      let pinfo = r[0];
+      pinfo.Category ={ID:pinfo.categoryid,Name:pinfo.Name,Detial:pinfo.Detial};
+      data.p = pinfo;
+      data.title = pinfo.alt + "-产品展示-广州捷邦"
+      let mainObj = {templatePath:'../product',data:{...data}}
+      data.mainObj = mainObj;
+      res.render('common/layout', data);
+    }).catch(err => {
+      let mainObj = {templatePath:'../product',data:{...data}}
+      data.mainObj = mainObj;
+      res.render('common/layout', data);
+    });
+});
+router.get('/contact', function (req, res, next) {
+  let pcDal = new ProductCategoryDal();
+  let pDal = new ProductDal();
+  let data = { pList: [], url: req.path,cList:[],category:{},title:'联系我们-广州捷邦节能设备制造有限公司'};
+  pcDal.Select(0, 100).then(
+    info => {
+      data.cList = info;
+      let mainObj = {templatePath:'../contact',data:{...data}}
+      data.mainObj = mainObj;
+      res.render('common/layout', data);
+    }).catch(err => {
+      let mainObj = {templatePath:'../contact',data:{...data}}
+      data.mainObj = mainObj;
+      res.render('common/layout', data);
+    });
+});
+
 router.get('/', function (req, res, next) {
   res.redirect("/index");
 })
